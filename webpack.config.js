8ad9d905b346950c3e9df84f6e9b04b298bf7dec
Vladimir Lila <Weslyg22@gmail.com>
2021-04-17 22:49:49 +0500

    browserlistrc and no target web = broken live reload)

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
+  target: 'web',
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
